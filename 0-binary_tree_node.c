700c3830c954aaad0e293ec272a1f5d8efaf56f4
jackeado <pabsanchez@gmail.com>
2020-03-12 12:53:50 -0500

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = new_node->right = NULL;
-    if (!parent)
-        parent = new_node;
-    return (new_node);
+	if (!parent)
+		parent = new_node;
+	return (new_node);
 }
