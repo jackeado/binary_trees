40e3f5d4b8bd29cbd7e585088a5701be8b4bd434
jackeado <pabsanchez@gmail.com>
2020-03-12 16:09:49 -0500

    Write a function that checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,19 +10,19 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-    {
-        return (0);
+	if (!node)
+	{
+		return (0);
 
-    }
+	}
 
 	if (!node->left && !node->right)
-    {
-        return (1);
+	{
+		return (1);
 
-    }
+	}
 	else
-    {
-        return (0);
-    }
+	{
+		return (0);
+	}
 }
